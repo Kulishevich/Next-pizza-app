967b7f5fc4eb6977077bf513049b101f20cba76d
Kulishevich <maxim8992012@gmail.com>
2024-08-29 17:30:38 +0300

    commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import { Container, Filters } from "@/components/shared";
+import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/top-bar";
+
 export default function Home() {
   return (
-    <main>
-      <h1>MAIN</h1>
-    </main>
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      
+      <TopBar/>
+
+      <Container className='mt-10 pb-14'>
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters/>
+          </div>
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col "gap-16>
+              Список товаров
+              {/* Prod */}
+              {/* Prod */}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,2 +1,7 @@
 export { Container } from './container'
 export { Header } from './header'
+export { Categories } from './categories'
+export { Title } from './title'
+export { SortPopup } from './sort-popup'
+export { Filters } from './filters'
+export { FilterCheckbox } from './filter-checkbox'
